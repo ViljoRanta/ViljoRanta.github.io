120333647f49b9e0ceb51d2c1e124ee4c9982c70
ViljoRanta <124779272+ViljoRanta@users.noreply.github.com>
2023-12-07 11:51:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-// App.js
-import React from 'react';
-import LoanList from './LoanList';
-import LoanForm from './LoanForm';
-import Aggregations from './Aggregations';
-import { LoanProvider } from './LoanContext';
-import './App.css';
-
-function App() {
-  return (
-    <LoanProvider>
-      <div className="app">
-        <h1>Loan Management App</h1>
-        <LoanForm />
-        <LoanList />
-        <Aggregations />
-      </div>
-    </LoanProvider>
-  );
-}
-
-export default App;
-
 // LoanContext.js
-import React, { createContext, useContext, useReducer } from 'react';
-
-const LoanContext = createContext();
+const LoanContext = React.createContext();
 
 const initialState = {
   loans: [],
@@ -37,15 +12,14 @@ const reducer = (state, action) => {
     case 'DELETE_LOAN':
       return { loans: state.loans.filter((loan) => loan.id !== action.payload) };
     case 'REDUCE_DEBT':
-      // Logic to reduce debt for a specific loan
-      return { loans: /* updated loan array */ };
+      return { loans:};
     default:
       return state;
   }
 };
 
 const LoanProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
   return (
     <LoanContext.Provider value={{ state, dispatch }}>
@@ -55,88 +29,17 @@ const LoanProvider = ({ children }) => {
 };
 
 const useLoanContext = () => {
-  return useContext(LoanContext);
+  return React.useContext(LoanContext);
 };
-
-export { LoanProvider, useLoanContext };
-
-// LoanForm.js
-import React, { useState } from 'react';
-import { useLoanContext } from './LoanContext';
-
-const LoanForm = () => {
-  const { dispatch } = useLoanContext();
-  const [loaner, setLoaner] = useState('');
-  const [debt, setDebt] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Validation logic
-    dispatch({ type: 'ADD_LOAN', payload: { id: Date.now(), loaner, debt: parseFloat(debt) } });
-    setLoaner('');
-    setDebt('');
-  };
-return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Loaner" value={loaner} onChange={(e) => setLoaner(e.target.value)} />
-      <input type="number" placeholder="Debt" value={debt} onChange={(e) => setDebt(e.target.value)} />
-      <button type="submit">Add Loan</button>
-    </form>
-  );
-};
-
-export default LoanForm;
-
-// LoanList.js
-import React from 'react';
-import { useLoanContext } from './LoanContext';
-
-const LoanList = () => {
-  const { state, dispatch } = useLoanContext();
-
-  const handleDelete = (id) => {
-    dispatch({ type: 'DELETE_LOAN', payload: id });
-  };
-
-  const handleReduceDebt = (id) => {
-    dispatch({ type: 'REDUCE_DEBT', payload: id });
-  };
-
+const App = () => {
   return (
-    <div>
-      <h2>Loan List</h2>
-      <ul>
-        {state.loans.map((loan) => (
-          <li key={loan.id}>
-            {loan.loaner} owes ${loan.debt}{' '}
-            <button onClick={() => handleDelete(loan.id)}>Delete</button>
-            <button onClick={() => handleReduceDebt(loan.id)}>Reduce Debt</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <LoanProvider>
+      <div className="app">
+        <h1>Loan Management App</h1>
+        {/ Include other components}
+      </div>
+    </LoanProvider>
   );
 };
 
-export default LoanList;
-
-// Aggregations.js
-import React from 'react';
-import { useLoanContext } from './LoanContext';
-
-const Aggregations = () => {
-  const { state } = useLoanContext();
-
-  const totalDebt = state.loans.reduce((sum, loan) => sum + loan.debt, 0);
-  const averageDebt = state.loans.length > 0 ? totalDebt / state.loans.length : 0;
-
-  return (
-    <div>
-      <h2>Aggregations</h2>
-      <p>Total Debt: ${totalDebt}</p>
-      <p>Average Debt: ${averageDebt}</p>
-    </div>
-  );
-};
-
-export default Aggregations;
+ReactDOM.render(<App />, document.getElementById('root'));
